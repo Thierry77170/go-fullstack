60471493892a7929b3fa133997a16ebc85ff78c2
Thierry DE SOUSA <thierry.desousa77@gmail.com>
2025-01-17 17:28:56 +0100

    update server.js

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,23 @@
 const express = require('express');
 const app = express();
+
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+});
+
+app.use((rq, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({message : "Votre requête a bien été reçu !"});
+    next();
+});
+
+app.use((req, res) => {
+    console.log('Réponse envoyée avec succès !');
+});
+
 module.exports = app;
