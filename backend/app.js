7c42b2526996aa82159898a5472246c0a544c588
Thierry DE SOUSA <thierry.desousa77@gmail.com>
2025-03-13 18:10:11 +0100

    configuring authentication routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 const app = express();
 
 mongoose.connect('mongodb+srv://thierry77:elanion07@cluster0.eran4.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0')
@@ -17,5 +18,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
