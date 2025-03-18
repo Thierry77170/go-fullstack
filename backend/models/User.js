03ee378a6b2e9d4b1ae47aac5750fb26c7276193
Thierry DE SOUSA <thierry.desousa77@gmail.com>
2025-03-18 15:42:32 +0100

    user creation

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
-const UserSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required }
+    password: { type: String, required: true }
 });
 
-UserSchema.plugin(uniqueValidator);
+userSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
