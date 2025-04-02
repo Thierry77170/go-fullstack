d007a13b48aa7c308e4630b14c6890a83a4e4581
Thierry DE SOUSA <thierry.desousa77@gmail.com>
2025-04-02 15:42:55 +0200

    configuring the authentication middleware

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post('/', stuffCtrl.createThing); 
-router.get('/', stuffCtrl.getAllStuff);
-router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
+router.post('/', auth, stuffCtrl.createThing); 
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
