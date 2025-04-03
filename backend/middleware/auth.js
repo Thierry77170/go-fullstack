7fe0f1e2155ed5d7bbedb4f116b9a3ad332987db
Thierry DE SOUSA <thierry.desousa77@gmail.com>
2025-04-03 20:37:05 +0200

    update Appli Express

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,13 @@
 const jwt = require('jsonwebtoken');
-
+ 
 module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        console.log('Token reçu :', token); // Ajoutez ce log
-
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-        console.log('Token décodé :', decodedToken); // Ajoutez ce log
-
-        const userId = decodedToken.userId;
-        req.auth = { userId };
-
-        next();
-    } catch (error) {
-        console.error('Erreur d\'authentification :', error); // Ajoutez ce log
-        res.status(401).json({ error: 'Requête non authentifiée !' });
-    }
+   try {
+       const token = req.headers.authorization.split(' ')[1];
+       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+       const userId = decodedToken.userId;
+       req.auth = { userId: userId };
+    next();
+   } catch(error) {
+       res.status(401).json({ error });
+   }
 };
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
 const auth = require('../middleware/auth');
 
-const router = express.Router();
-
-router.post('/', auth, stuffCtrl.createThing); 
 router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, stuffCtrl.createThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
 router.put('/:id', auth, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
